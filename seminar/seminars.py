24faabe2a40f1467953937c661d8cadd02a651c0
bhm699 <bhm699@cs.bham.ac.uk>
2017-10-28 22:38:45 +0100

    tags timegit add . :D

--- seminar/seminars.py
+++ seminar/seminars.py
@@ -1,4 +1,5 @@
 import nltk
+import re
 from os import listdir
 from os.path import isfile, join
 mypath = "seminars_training"
@@ -6,7 +7,43 @@ onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 corpus_root = mypath
 corpus = nltk.corpus.reader.plaintext.PlaintextCorpusReader(corpus_root, onlyfiles)
 print(corpus.words())
-corpus2 = nltk.data.load('seminars_training/0.txt')
-print(corpus2)
+
+#Load the text file to tag
+corpus2 = nltk.data.load('seminars_training/258.txt')
+#corpus2 = nltk.data.load('seminars_untagged/301.txt')
+print(corpus2) #print text just for purposes of checking
+
+#tokenise text
 tokens = nltk.word_tokenize(corpus2)
-print(tokens)
+
+#list of regex used
+timeReg = '([0-9]+(\s|:|pm|am){1}[0-9]*$)'
+
+#word index for for loop
+index = 0
+
+#tags any times correctly
+def tagTime(word , index):
+    printWord = " "
+    nextWord = tokens[index+1].upper()
+    prevWord = tokens[index-1]
+    print("HI " + prevWord)
+    if (prevWord == '-') :
+        if (nextWord == 'AM' or nextWord == 'PM') :
+            printWord = '<etime>' + word + nextWord + '</etime>'
+        else :
+            printWord = '<etime>' + word + '</etime>'
+    elif(nextWord == 'AM' or nextWord == 'PM') :
+        printWord = ('<stime>' + word + nextWord + '</stime>')
+    else :
+        printWord = '<stime>' + word + '</stime>'
+    return printWord
+
+#loops through text and if it needs to be tagged calls correct method to tag it
+for word in tokens :
+    if (word.upper() != 'AM' or word.upper() != 'PM'):
+        if re.match(timeReg, word):
+            print(tagTime(word, index))
+        else :
+            print(word)
+    index = index + 1
