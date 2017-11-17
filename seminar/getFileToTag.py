928290694450c058cf1a7669e8549e2e4e0a10e5
bhm699 <bhm699@cs.bham.ac.uk>
2017-11-17 12:16:25 +0000

    trying to do paragraphs etc

--- seminar/getFileToTag.py
+++ seminar/getFileToTag.py
@@ -1,15 +1,19 @@
+import re
 import sys
 import nltk
+import nltk.data
 from nltk.tokenize import sent_tokenize, word_tokenize
 
 '''
 This file is used to get the file to be tagged
 Will be editted so that a user can choose a file to tag
 '''
-mypath = "seminars_training/8test.txt"
-'''def getFileToTag (userFile) :
-    mypath = "seminars_training/" + userFile + ".txt"    
-'''
+mypath = ""
+print("Enter the number of the file you would like to tag : ");
+userFile = input()
+mypath = "seminars_training/" + userFile + ".txt"  
+
+print("b : " + mypath)
 #Load the text file to tag
 corpus2 = nltk.data.load(mypath)
 print(corpus2) #print text just for purposes of checking
@@ -24,3 +28,20 @@ def getSentences():
 #get tokens - for other files
 def getTokens() :
     return tokens
+
+############
+abstractReg = re.compile("[Aa]bstract\:\s*")
+def doStuffWithData():
+    with open("seminars_training/8test.txt", "r") as file :
+        corpus = file.read()
+        #theLines = corpus.split("\n")
+        print(corpus)
+        paragraphs = []
+        paragraphs = corpus.split("\n\n")
+        print("----paras------")
+        print(paragraphs)
+        for paragraph in paragraphs :
+            paragraph= re.sub("\n\n", "<paragraph> " + paragraph + " </paragraph>", paragraph)
+        
+        
+    
