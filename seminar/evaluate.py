1a309b956470a8f5105f239bef720f6c272f7f5c
bhm699 <bhm699@cs.bham.ac.uk>
2017-12-05 00:15:06 +0000

    added printing of averages of all tags and more txt files

--- seminar/evaluate.py
+++ seminar/evaluate.py
@@ -52,6 +52,11 @@ def getFN(actual, generated):
 notEnd = True
 textFileID = 301
 overallAccuracy = 0
+maxAcc = 0
+overallSpeakAcc = 0
+overallLocAcc = 0
+overallSTimeAcc = 0
+overallETimeAcc = 0
 fileNumber = 0
 
 while(notEnd) :
@@ -77,33 +82,43 @@ while(notEnd) :
     removeSecond(STime)
     removeSecond(ETime)
 
-    print("actual")
-    print(taggedSpeakers)
-    print(taggedLoc)
-    print(taggedSTime)
-    print(taggedETime)
-    print("tagged")
-    print(Speakers)
-    print(Location)
-    print(STime)
-    print(ETime)
-
+    accSpeakers = (getTP(taggedSpeakers, Speakers) + getTN(taggedSpeakers, Speakers)) / (getTP(taggedSpeakers, Speakers) + getTN(taggedSpeakers, Speakers) + getFP(taggedSpeakers, Speakers) + getFN(taggedSpeakers, Speakers))
+    accLoc = (getTP(taggedSpeakers, Speakers) + getTN(taggedLoc, Speakers)) / (getTP(taggedLoc, Speakers) + getTN(taggedLoc, Speakers) + getFP(taggedLoc, Speakers) + getFN(taggedLoc, Speakers))
+    accSTime = (getTP(taggedSpeakers, Speakers) + getTN(taggedSTime, Speakers)) / (getTP(taggedSTime, Speakers) + getTN(taggedSTime, Speakers) + getFP(taggedSTime, Speakers) + getFN(taggedSTime, Speakers))
+    accETime = (getTP(taggedSpeakers, Speakers) + getTN(taggedETime, Speakers)) / (getTP(taggedETime, Speakers) + getTN(taggedETime, Speakers) + getFP(taggedETime, Speakers) + getFN(taggedETime, Speakers))
 
     TP = getTP(taggedSpeakers, Speakers) + getTP(taggedLoc, Location) + getTP(taggedSTime, STime) + getTP(taggedETime, ETime) 
     TN = getTN(taggedSpeakers, Speakers) + getTN(taggedLoc, Location) + getTN(taggedSTime, STime) + getTN(taggedETime, ETime) 
     FP = getFP(taggedSpeakers, Speakers) + getFP(taggedLoc, Location) + getFP(taggedSTime, STime) + getFP(taggedETime, ETime) 
     FN = getFN(taggedSpeakers, Speakers) + getFN(taggedLoc, Location) + getFN(taggedSTime, STime) + getFN(taggedETime, ETime) 
 
-    accuracy = (TP + TN) / (TP + TN + FP + FN) 
+    accuracy = (TP + TN) / (TP + TN + FP + FN)
+    if (accuracy > maxAcc):
+        maxAcc = accuracy
+    
     print("accuracy for text file " + str(textFileID) + ": " + str(accuracy*100))
+    print("Speaker accuracy " + str(accSpeakers*100))
+    print("Location accuracy " + str(accLoc*100))
+    print("Start Time accuracy " + str(accSTime*100))
+    print("End Time accuracy " + str(accETime*100))
+    print("--------------------------------------------------")
     fileNumber = fileNumber + 1
     overallAccuracy = overallAccuracy + accuracy
-    if(textFileID == 330):
+    overallSpeakAcc = overallSpeakAcc + accSpeakers
+    overallLocAcc = overallLocAcc + accLoc
+    overallSTimeAcc = overallSTimeAcc + accSTime
+    overallETimeAcc = overallETimeAcc + accETime
+    if(textFileID == 360):
         notEnd = False
     else :
         textFileID = textFileID + 1
 
 print("Overall Accuracy : " + str(overallAccuracy * 100 / fileNumber))
+print("Speaker Accuracy : " + str(overallSpeakAcc * 100 / fileNumber))
+print("Location Accuracy : " + str(overallLocAcc * 100 / fileNumber))
+print("Start Time Accuracy : " + str(overallSTimeAcc * 100 / fileNumber))
+print("End Time Accuracy : " + str(overallETimeAcc * 100 / fileNumber))
+print("Highest Overall Accuracy: " + str(maxAcc*100))
 '''
 precision = (TPclass) / (classified)
 recall = (TPclass) / (TP_in_corpus)
